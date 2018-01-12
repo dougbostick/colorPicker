8cf60ac731fb5005362c5945f65e1ea771ff8ef5
Thomas Kelly <tmkelly28@gmail.com>
2018-01-12 09:30:52 -0500

    add slightly more starting point

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
 // Your code here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Picker extends React.Component {
+  render () {
+    return (
+      <div id='container'>
+        <div id='navbar'>
+          <div>Currently selected: </div>
+          <div className='red'>red</div>
+        </div>
+        <div id='colors-list'>
+          {/* colors go here */}
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Picker />,
+  document.getElementById('app')
+)
